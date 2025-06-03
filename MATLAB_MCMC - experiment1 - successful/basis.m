a72c26399327f5c5d9ee35717549a3084d390c2b
puzhaokow1993 <84223927+puzhaokow1993@users.noreply.github.com>
2025-06-03 10:00:52 +0800

    Delete MATLAB_MCMC - experiment1 - successful directory

--- MATLAB_MCMC - experiment1 - successful/basis.m
+++
@@ -1,3 +0,0 @@
-function z = basis(x,y,r,s,ell_max) 
-z = (0<(2^(ell_max))*(x+1)-r).*((2^(ell_max))*(x+1)-r<1).*(0<(2^(ell_max))*(y+1)-s).*((2^(ell_max))*(y+1)-s<1); 
-end
--- MATLAB_MCMC - experiment1 - successful/log_likelihood_8_workers.m
+++
@@ -1,7 +0,0 @@
-function ell = log_likelihood_8_workers(k,N,XY_coordinates,measurements,coefficient_F,angles,sigma)  
-Gf = zeros(1,N); 
-for h=1:(N/8)
-    Gf(1,8*h-7:8*h) = param_forward_map_8_workers(k,angles(2,8*h-7:8*h),angles(1,8*h-7:8*h),coefficient_F,XY_coordinates); 
-end
-ell = -0.5*((norm(measurements-Gf)/sigma)^2); 
-end 
