31d543d19db7fda30171fe6fdd73589c8072ab03
puzhaokow1993 <84223927+puzhaokow1993@users.noreply.github.com>
2025-06-03 10:01:40 +0800

    Delete MATLAB_MCMC - experiment3 - successful directory

--- MATLAB_MCMC - experiment3 - successful/log_likelihood.m
+++
@@ -1,7 +0,0 @@
-function ell = log_likelihood(k,N,XY_coordinates,measurements,coefficient_F,angles,sigma)  
-Gf = zeros(1,N); 
-for h=1:N
-    Gf(1,h) = param_forward_map(k,angles(2,h),angles(1,h),coefficient_F,XY_coordinates); 
-end
-ell = -0.5*((norm(measurements-Gf)/sigma)^2); 
-end 
--- MATLAB_MCMC - experiment3 - successful/n_true_1.m
+++
@@ -1,3 +0,0 @@
-function z = n_true_1(x,y)
-z = 1 + (x<1/2).*(x+y>-1/4).*(y<1/2)./10;  
-end
